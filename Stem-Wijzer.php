461ccea6254d8c17dfb8e7627521a47fd061e4c3
Max Scholten <PS229429@summacollege.nl>
2023-06-19 14:07:19 +0200

    test

--- Stem-Wijzer.php
+++ Stem-Wijzer.php
@@ -17,3 +17,58 @@ if ($conn->connect_error) {
 // Close the database connection
 $conn->close();
 ?>
+
+<?php
+// Database of parties and their positions
+$parties = [
+    'VVD' => [
+        'De provincie moet velden met zonnepanelen op landbouwgrond verbieden.' => false,
+        'De provincie hoeft geen haast te maken met maatregelen die stikstofuitstoot verminderen.' => true,
+        'De provincie moet meer geld uitgeven aan trainingsmogelijkheden voor getalenteerde topsporters.' => false,
+        'Maastricht Aachen Airport moet gesloten worden.' => false
+    ],
+    'D66' => [
+        'De provincie moet velden met zonnepanelen op landbouwgrond verbieden.' => false,
+        'De provincie hoeft geen haast te maken met maatregelen die stikstofuitstoot verminderen.' => false,
+        'De provincie moet meer geld uitgeven aan trainingsmogelijkheden voor getalenteerde topsporters.' => true,
+        'Maastricht Aachen Airport moet gesloten worden.' => false
+    ],
+    'SP' => [
+        'De provincie moet velden met zonnepanelen op landbouwgrond verbieden.' => true,
+        'De provincie hoeft geen haast te maken met maatregelen die stikstofuitstoot verminderen.' => false,
+        'De provincie moet meer geld uitgeven aan trainingsmogelijkheden voor getalenteerde topsporters.' => true,
+        'Maastricht Aachen Airport moet gesloten worden.' => true
+    ],
+    // Add more parties and their positions here...
+];
+
+// Storing user's answers
+$userAnswers = [
+    'De provincie moet velden met zonnepanelen op landbouwgrond verbieden.' => true,
+    'De provincie hoeft geen haast te maken met maatregelen die stikstofuitstoot verminderen.' => false,
+    'De provincie moet meer geld uitgeven aan trainingsmogelijkheden voor getalenteerde topsporters.' => true,
+    'Maastricht Aachen Airport moet gesloten worden.' => false
+];
+
+// Calculate the scores for each party
+$scores = [];
+foreach ($parties as $party => $positions) {
+    $score = 0;
+    foreach ($positions as $question => $positionValue) {
+        if ($positionValue === $userAnswers[$question]) {
+            $score++;
+        }
+    }
+    $scores[$party] = $score;
+}
+
+// Sort the parties based on the scores
+arsort($scores);
+
+// Display the results
+echo "Results:\n";
+foreach ($scores as $party => $score) {
+    echo $party . ": " . $score . " points\n";
+}
+?>
+
