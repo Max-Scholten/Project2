4c9a83b2358a53370c2885fd69db281850b679d0
MaraGreuter <ps239894@summacollege.nl>
2023-06-23 11:02:56 +0200

    link rel stylesheet

--- questionnaire.php
+++ questionnaire.php
@@ -48,7 +48,7 @@ $conn->close();
 <html>
 <head>
     <title>Questionnaire</title>
-    <link type="stylesheet" href="../Project2/css/question.css">
+    <link rel="stylesheet" href="../Project2/css/question.css">
 </head>
 <body>
     <h2>Questionnaire</h2>
