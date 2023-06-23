29821fc25861f8193324a7736be2d50cc2070d1a
aliciavh <ps243032@summacollege.nl>
2023-06-23 14:50:14 +0200

    register message deleted

--- login.php
+++ login.php
@@ -40,11 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':password', $password);
-        if ($stmt->execute()) {
-            $successMessage = "User registered successfully. Please login.";
-        } else {
-            $errorMessage = "Error registering user: " . $stmt->errorInfo()[2];
-        }
+       
     }
 }
 
