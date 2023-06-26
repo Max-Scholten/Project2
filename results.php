2b83977289e8de57dc73f9f7041536f26f805cf8
Max Scholten <PS229429@summacollege.nl>
2023-06-26 08:34:05 +0200

    file path changes includ partys files

--- results.php
+++ results.php
@@ -10,15 +10,15 @@
         <h2>Your Best Matching Party:</h2>
         <div>
             <h3>VVD</h3>
-            <p><?php include('vvd.php'); ?></p>
+            <p><?php include('Calculatour/vvd.php'); ?></p>
         </div>
         <div>
             <h3>D66</h3>
-            <p><?php include('sp.php'); ?></p>
+            <p><?php include('Calculatour/sp.php'); ?></p>
         </div>
         <div>
             <h3>SP</h3>
-            <p><?php include('d66.php'); ?></p>
+            <p><?php include('Calculatour/d66.php'); ?></p>
         </div>
     </div>
     <form method="post" action="login.php">
